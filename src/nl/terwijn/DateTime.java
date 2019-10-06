d05a203ecaab598410a5d9e76250a1b0e9a5f749
Tim Terwijn <TimTerwijn@users.noreply.github.com>
2019-10-06 13:56:03 +0200

    DateTime done

---
+++ src/nl/terwijn/DateTime.java
@@ -0,0 +1,15 @@
+package nl.terwijn;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTime {
+
+    private DateTime(){}
+
+    public static String now(){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        return formatter.format(date);
+    }
+}
